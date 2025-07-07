672594e945a9e64ed996dca589cb9a7185a66712
upesnavneet <kumarnavneet7765@gmail.com>
2025-07-07 17:21:49 +0530

    idk what atp commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,46 @@
-
 import Particles from '../Background1/Particles/Particles';
 import Home from './home';
 import Navbar from './Navbar';
 import './App.css';
 import About from './About';
-import Robot from './Robot';
 import Initiatives from './Initiatives';
+import Robot from './Robot';
 import './index.css';
-
-
 
 function App() {
   return (
     <>
- 
-        <Navbar />
+      <Navbar />
 
-        <main className="scroll-content">
-          
-            <section className="particles-section">
-            <Home />
-            <Particles
-              particleColors={['#00000000']}
-              particleCount={200}
-              particleSpread={10}
-              speed={0.1}
-              particleBaseSize={100}
-              moveParticlesOnHover={true}
-              alphaParticles={false}
-              disableRotation={false}
-            />
-            </section>
+      <main className="scroll-content">
 
-           <section className="about-section" id="about">
-              <About />
-            </section>
+        <section className="particles-section">
+          <Home />
+          <Particles
+            particleColors={['#00000000']}
+            particleCount={200}
+            particleSpread={10}
+            speed={0.1}
+            particleBaseSize={100}
+            moveParticlesOnHover={true}
+            alphaParticles={false}
+            disableRotation={false}
+          />
+        </section>
 
-           <section className="initiatives-section" id="initiatives">
-             <Initiatives />
-           </section>
+        <section className="about-section" id="about">
+          <About />
+        </section>
 
-           <section className="robot-section" id="robot">
-              <Robot />
-           </section>
+        <section className="initiatives-section" id="initiatives">
+          <Initiatives />
+        </section>
 
-        </main>
+        <section className="robot-section" id="robot">
+          <Robot /> 
+        </section>
 
+      </main>
     </>
   );
 }
